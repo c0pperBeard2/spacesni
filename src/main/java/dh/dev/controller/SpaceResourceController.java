e5316bbd8ace689afed7432831f3c21a2b889deb
David Houston <davidaehouston@gmail.com>
2024-02-25 18:41:43 +0000

    Creation of basic controller method and serivce call

--- src/main/java/dh/dev/controller/SpaceResourceController.java
+++ src/main/java/dh/dev/controller/SpaceResourceController.java
@@ -1,8 +1,22 @@
 package dh.dev.controller;
 
+import dh.dev.model.SpaceResource;
+import dh.dev.service.SpaceResourceService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SpaceResourceController {
 
+    private final SpaceResourceService spaceResourceService;
+
+    public SpaceResourceController(SpaceResourceService spaceResourceService) {
+        this.spaceResourceService = spaceResourceService;
+    }
+
+    public ResponseEntity<List<SpaceResource>> getAllSpaces() {
+        return ResponseEntity.ok(spaceResourceService.getAllSpaces());
+    }
 }
--- src/main/java/dh/dev/service/SpaceResourceService.java
+++ src/main/java/dh/dev/service/SpaceResourceService.java
@@ -1,8 +1,15 @@
 package dh.dev.service;
 
+import dh.dev.model.SpaceResource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SpaceResourceService {
 
+    public List<SpaceResource> getAllSpaces() {
+        // TODO - Establish Data Source for Repository
+        return null;
+    }
 }
