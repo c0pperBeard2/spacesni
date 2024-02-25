28c8cb09edb6af39b280f3e098ad5655ff837fc1
David Houston <davidaehouston@gmail.com>
2024-02-25 16:02:22 +0000

    base commit

---
+++ src/main/java/dh/dev/SpacesNIApplication.java
@@ -0,0 +1,13 @@
+package dh.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacesniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacesniApplication.class, args);
+	}
+
+}
